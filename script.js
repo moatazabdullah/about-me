dd0f8e404209f65cac3823e02c745f14440a6884
Moataz Abdullah <abdullahmoataz90@gmail.com>
2023-05-26 20:58:59 +0300

    fifth commit

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const outputDiv = document.getElementById("terminal-output");
   const commandInput = document.getElementById("command-input");
 
   function displayWelcomeMessage() {
     const welcomeMessage = "Welcome to My Terminal!\nType 'help' to see the available commands.";
-    const message = document.createElement("pre");
-    message.textContent = welcomeMessage;
-    outputDiv.appendChild(message);
+    displayText(welcomeMessage);
   }
 
   function displayHelpMessage() {
@@ -15,44 +13,50 @@ document.addEventListener("DOMContentLoaded", function() {
       "- whoami : Display information about me\n" +
       "- findme : Find me on social media\n"+
       "- Clear OR CTRL + L: Clears the screen";
-    const message = document.createElement("pre");
-    message.textContent = helpMessage;
-    outputDiv.appendChild(message);
+    displayText(helpMessage);
   }
 
   function handleCommand(command) {
-    const output = document.createElement("p");
-    output.textContent = "$ " + command;
-    outputDiv.appendChild(output);
+    displayText("$ " + command);
 
-    if (command === "help") {
-      displayHelpMessage();
-    } else if (command === "clear") {
-      clearScreen();
-    } else if (command === "whoami") {
-      displayWhoAmI();
-    } else if (command === "findme") {
-      displayFindMe();
-    } else {
-      displayErrorMessage(command);
+    switch (command) {
+      case "help":
+        displayHelpMessage();
+        break;
+      case "clear":
+        clearScreen();
+        break;
+      case "whoami":
+        displayWhoAmI();
+        break;
+      case "findme":
+        displayFindMe();
+        break;
+      default:
+        displayErrorMessage(command);
+        break;
     }
   }
 
-  function displayWhoAmI() {
-    const message = document.createElement("p");
-    message.textContent = "I'm Moataz a Networking and Telecommunication Engineer and   a cybersecurity enthusiast driven by a deep passion for the field. Constantly seeking to develop my skills and knowledge, I am dedicated to staying updated with the latest trends and advancements in cybersecurity. My expertise lies in penetration testing, where I specialize in conducting thorough assessments of systems and networks to identify vulnerabilities. Additionally, I have proficiency in web application pentesting, ensuring the security of online platforms. Programming languages such as C++ and Python are my tools of choice, allowing me to develop effective and efficient solutions. With a relentless drive for improvement, I am committed to making a positive impact in the realm of cybersecurity.";
+  function displayText(text) {
+    const message = document.createElement("pre");
+    message.textContent = text;
     outputDiv.appendChild(message);
   }
 
+  function displayWhoAmI() {
+    const message = "I'm Moataz, a Networking and Telecommunication Engineer and a cybersecurity enthusiast driven by a deep passion for the field. Constantly seeking to develop my skills and knowledge, I am dedicated to staying updated with the latest trends and advancements in cybersecurity. My expertise lies in penetration testing, where I specialize in conducting thorough assessments of systems and networks to identify vulnerabilities. Additionally, I have proficiency in web application pentesting, ensuring the security of online platforms. Programming languages such as C++ and Python are my tools of choice, allowing me to develop effective and efficient solutions. With a relentless drive for improvement, I am committed to making a positive impact in the realm of cybersecurity.";
+    displayTextWithTypingEffect(message);
+  }
+
   function displayFindMe() {
-    const message = document.createElement("p");
-    message.innerHTML =
-      "<strong>You can find me on:</strong><br><br>" +
-      "- <a href='https://example.com'>Website</a><br>" +
-      "- <a href='https://linkedin.com/[your_username]'>LinkedIn</a><br>" +
-      "- <a href='https://github.com/[your_username]'>GitHub</a><br>" +
-      "- Email: youremail@example.com";
-    outputDiv.appendChild(message);
+    const message =
+      "You can find me on:\n\n" +
+      "- Website: [your website URL]\n" +
+      "- LinkedIn: [your LinkedIn profile URL]\n" +
+      "- GitHub: [your GitHub profile URL]\n" +
+      "- Email: [your email address]";
+    displayText(message);
   }
 
   function clearScreen() {
@@ -60,12 +64,29 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
   function displayErrorMessage(command) {
-    const message = document.createElement("p");
-    message.textContent = `Command not found: ${command}`;
-    outputDiv.appendChild(message);
+    const message = `Command not found: ${command}`;
+    displayText(message);
   }
 
-  document.addEventListener("keydown", function(event) {
+  function displayTextWithTypingEffect(text) {
+    const characters = text.split("");
+    const output = document.createElement("pre");
+    outputDiv.appendChild(output);
+
+    let index = 0;
+    const typingSpeed = 50;
+
+    const typingInterval = setInterval(() => {
+      output.textContent += characters[index];
+      index++;
+
+      if (index >= characters.length) {
+        clearInterval(typingInterval);
+      }
+    }, typingSpeed);
+  }
+
+  document.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
       event.preventDefault();
       handleCommand(commandInput.value);
@@ -76,8 +97,6 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   });
 
-  // Call the displayWelcomeMessage function when the page loads
   displayWelcomeMessage();
-
   commandInput.focus();
 });
