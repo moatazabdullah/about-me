2e2ef691e5f759ff283f1e1b32cbcfe5cce16f8e
Moataz Abdullah <abdullahmoataz90@gmail.com>
2023-05-26 22:45:45 +0300

    final commit

--- script.js
+++ script.js
@@ -3,21 +3,21 @@ document.addEventListener("DOMContentLoaded", function () {
   const commandInput = document.getElementById("command-input");
 
   function displayWelcomeMessage() {
-    const welcomeMessage = "Welcome to My Terminal!\nType 'help' to see the available commands.";
-    displayText(welcomeMessage);
+    const welcomeMessage = "Welcome to My Website!\nType 'help' to see the available commands.";
+    displayTextWithTypingEffect(welcomeMessage, 15);
   }
 
   function displayHelpMessage() {
     const helpMessage =
       "Available commands:\n" +
       "- whoami : Display information about me\n" +
-      "- findme : Find me on social media\n"+
+      "- findme : Find me on social media\n" +
       "- Clear OR CTRL + L: Clears the screen";
-    displayText(helpMessage);
+    displayTextWithTypingEffect(helpMessage, 15);
   }
 
   function handleCommand(command) {
-    displayText("$ " + command);
+    displayText("root@moataz$ " + command);
 
     switch (command) {
       case "help":
@@ -38,29 +38,28 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-function displayText(text) {
-  const outputContainer = document.getElementById("terminal-output");
-  const message = document.createElement("pre");
-  message.textContent = text;
-  outputContainer.appendChild(message);
-  outputContainer.scrollTop = outputContainer.scrollHeight;
-}
-
-
+  function displayText(text) {
+    const outputContainer = document.getElementById("terminal-output");
+    const message = document.createElement("pre");
+    message.textContent = text;
+    outputContainer.appendChild(message);
+    outputContainer.scrollTop = outputContainer.scrollHeight;
+  }
 
   function displayWhoAmI() {
-    const message = "I'm Moataz, a Networking and Telecommunication Engineer and a cybersecurity enthusiast driven by a deep passion for the field.\n"+"Constantly seeking to develop my skills and knowledge, I am dedicated to staying updated with the latest trends and advancements in cybersecurity."+ "My expertise lies in penetration testing, where I specialize in conducting thorough assessments of systems and networks to identify vulnerabilities." + "Additionally, I have proficiency in web application pentesting, ensuring the security of online platforms." + " Programming languages such as C++ and Python are my tools of choice, allowing me to develop effective and efficient solutions." + " With a relentless drive for improvement, I am committed to making a positive impact in the realm of cybersecurity.";
-    displayTextWithTypingEffect(message);
+    const message = "Hi,\nI'm Moataz, a Networking and Telecommunication Engineer and a cybersecurity enthusiast driven by a deep passion for the field.\nConstantly seeking to develop my skills and knowledge, I am dedicated to staying updated with the latest trends and advancements in cybersecurity.\nMy expertise lies in penetration testing, where I specialize in conducting thorough assessments of systems and networks to identify vulnerabilities.\nAdditionally, I have proficiency in web application pentesting, ensuring the security of online platforms. Programming languages such as C++ and Python are my tools of choice,\nallowing me to develop effective and efficient solutions. With a relentless drive for improvement, I am committed to making a positive impact in the realm of cybersecurity.";
+    displayTextWithTypingEffect(message, 10);
   }
 
   function displayFindMe() {
-    const message =
-      "You can find me on:\n\n" +
-      "- Website: [your website URL]\n" +
-      "- LinkedIn: [your LinkedIn profile URL]\n" +
-      "- GitHub: [your GitHub profile URL]\n" +
-      "- Email: [your email address]";
-    displayText(message);
+    const message = document.createElement("p");
+    message.innerHTML =
+      "<strong>You can find me on:</strong><br><br>" +
+      "- <a href='https://moatazabdullah.github.com/about-me'>Website</a><br>" +
+      "- <a href='https://linkedin.com/moataz-abdullah'>LinkedIn</a><br>" +
+      "- <a href='https://github.com/moatazabdullah'>GitHub</a><br>" +
+      "- Email: abdullahmoataz90@gmail.com";
+    outputDiv.appendChild(message);
   }
 
   function clearScreen() {
@@ -69,16 +68,16 @@ function displayText(text) {
 
   function displayErrorMessage(command) {
     const message = `Command not found: ${command}`;
-    displayText(message);
+    displayTextWithTypingEffect(message);
   }
 
-  function displayTextWithTypingEffect(text) {
+  function displayTextWithTypingEffect(text, s) {
     const characters = text.split("");
     const output = document.createElement("pre");
     outputDiv.appendChild(output);
 
     let index = 0;
-    const typingSpeed = 50;
+    const typingSpeed = s;
 
     const typingInterval = setInterval(() => {
       output.textContent += characters[index];
@@ -104,3 +103,4 @@ function displayText(text) {
   displayWelcomeMessage();
   commandInput.focus();
 });
+
